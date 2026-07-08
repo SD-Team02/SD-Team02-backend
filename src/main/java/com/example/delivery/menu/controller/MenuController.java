7292393e2d1482100e94b4530c70bf12acbb3cc7
TaeHeeKim <eksrnsdhkdwk@gmail.com>
2026-07-08 12:52:53 +0900

    [feat] - 메뉴 생성 조회 수정 삭제 구현 완료, AI 히스토리 생성 완료

--- src/main/java/com/example/delivery/menu/controller/MenuController.java
+++ src/main/java/com/example/delivery/menu/controller/MenuController.java
@@ -8,16 +8,12 @@ import com.example.delivery.menu.dto.response.MenuResponseDto;
 import com.example.delivery.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
