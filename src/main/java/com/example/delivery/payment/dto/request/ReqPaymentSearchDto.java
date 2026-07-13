03921c399d3d4cc4478c22f23b32b4f174e3dda0
choipg <choipg2684@naver.com>
2026-07-13 11:26:25 +0900

    refactor: @Notnull 추가 (#10)

--- src/main/java/com/example/delivery/payment/dto/request/ReqPaymentSearchDto.java
+++ src/main/java/com/example/delivery/payment/dto/request/ReqPaymentSearchDto.java
@@ -2,6 +2,7 @@ package com.example.delivery.payment.dto.request;
 
 import com.example.delivery.payment.entity.PaymentStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,12 +14,14 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class ReqPaymentSearchDto {
 
-    @Schema(description = "검색 시작 날짜", example = "2026-07-08")
+    @NotNull(message = "검색 시작 날짜는 필수 입력 사항입니다.")
+    @Schema(description = "검색 시작 날짜", example = "2026-07-12")
     private LocalDate startDate;
 
-    @Schema(description = "검색 종료 날짜", example = "2026-07-09")
+    @NotNull(message = "검색 종료 날짜는 필수 입력 사항입니다.")
+    @Schema(description = "검색 종료 날짜", example = "2026-07-13")
     private LocalDate endDate;
-
+ㄴ
     @Schema(description = "결제 상태 필터링", example = "SUCCESS")
     private PaymentStatus status;
 
