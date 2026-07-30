2b1c955892288b31cff232fbdb60d093651af6c0
TaeHeeKim <eksrnsdhkdwk@gmail.com>
2026-07-30 10:13:22 +0900

    [fix] - 기능 오류 수정 및 주석 추가

--- src/main/java/com/example/delivery/menu/dto/request/AiGenerateRequestDto.java
+++ src/main/java/com/example/delivery/menu/dto/request/AiGenerateRequestDto.java
@@ -14,10 +14,10 @@ public class AiGenerateRequestDto {
     // AI 답변생성
     @NotNull(message = "메뉴 이미지 ID는 필수입니다.")
     private UUID imageId;               // 이미지 전송
-    @Size(max = 300, message = "요청 텍스트는 300자 이하로 입력해주세요.")
     @NotBlank (message = "메뉴 이름은 필수입니다.")
     private String menuName;            // 메뉴 이름
     @NotBlank(message = "프롬프트는 필수입니다.")
+    @Size(max = 300, message = "요청 텍스트는 300자 이하로 입력해주세요.")
     private String prompt;              // AI에게 명령한 프롬프트
     private String category;            // (한식, 중식, 일식....)
 
